898c6a732fba3f959d81b5f364d8bc2b88e4dd3c
zhangchao <529743595@qq.com>
2020-06-07 21:05:23 +0800

    Add Spawn

--- child_process.js
+++ child_process.js
@@ -1,11 +1,10 @@
 const child_process = require('child_process')
 
-const { execFile } = child_process
+const { spawn } = child_process
 
-const userInput = '-al && pwd' // '-al && rm -fm *'
+const userInput = '.' // '-al && rm -fm *'
 const options = { pwd: 'C:\\', env: { NODE_ENV: 'development' } }
-execFile('ls', ['-al', userInput], options, (error, stdout, stderr) => {
-  console.log(error)
-  console.log(stdout)
-  console.log(stderr)
+const streams = spawn('ls', ['-al', userInput], options)
+streams.stdout.on('data', chunk => {
+  console.log(chunk.toString())
 })
